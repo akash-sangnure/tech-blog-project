f30d533c89e523cbcd6f61fff9145c1275a0d5c8
AdityaVSM <vsmcodes55@gmail.com>
2020-12-10 20:37:22 +0530

    added placeholder in input field of login.php

--- login.php
+++ login.php
@@ -12,24 +12,16 @@
 		if(!empty($user_name) && !empty($password)){
 			//read from database
 			$query = "select * from users where user_name = '$user_name' limit 1";
-
 			$result = mysqli_query($con,$query);
-
 			if($result && mysqli_num_rows($result) > 0){
-
 				$user_data = mysqli_fetch_assoc($result);
-
 				if($user_data['password'] === $password){
-
 					$_SESSION['user_id'] = $user_data['user_id'];
 					header("Location: index.php");
 					die(); 
-
 				}
-
 			}
 			echo "Wrong username or password!";
-			
 		}else{
 			echo "Please enter valid information";
 		}
