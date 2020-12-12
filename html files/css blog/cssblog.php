74cab91945fe0f5114458d76538cc74260d8eebe
AdityaVSM <vsmcodes55@gmail.com>
2020-12-12 18:33:38 +0530

    added content to cssblog.php

--- html files/css blog/cssblog.php
+++ html files/css blog/cssblog.php
@@ -39,8 +39,43 @@
             </div>
             <div class="grid-child-content">
             <p >
-                
+            <br>CSS is used to control the style of a web document in a simple and easy way.
+            CSS is the acronym for "Cascading Style Sheet". This tutorial covers both the versions CSS1,CSS2 and CSS3, and gives a complete understanding of CSS, starting from its basics to advanced concepts.
+
+            <div style="text-align: center;"><br><strong><h3>Why to Learn CSS?</h3></strong><br></div>
+            Cascading Style Sheets, fondly referred to as CSS, is a simple design language intended to simplify the process of making web pages presentable.
+            CSS is a MUST for students and working professionals to become a great Software Engineer specially when they are working in Web Development Domain. I will list down some of the key advantages of learning CSS:
+            <ul>
+                <li><strong>Create Stunning Web site</strong> - CSS handles the look and feel part of a web page. Using CSS, you can control the color of the text, the style of fonts, the spacing between paragraphs, how columns are sized and laid out, what background images or colors are used, layout designs,variations in display for different devices and screen sizes as well as a variety of other effects.</li>
+                <li><strong>Become a web designer</strong>  - If you want to start a carrer as a professional web designer, HTML and CSS designing is a must skill.</li>
+                <li><strong>Control web</strong>  - CSS is easy to learn and understand but it provides powerful control over the presentation of an HTML document. Most commonly, CSS is combined with the markup languages HTML or XHTML.</li>
+                <li><strong>Learn other languages</strong>  - Once you understands the basic of HTML and CSS then other related technologies like javascript, php, or angular are become easier to understand.</li>
+            </ul>
+
+            <div style="text-align: center;"><br><strong><h3>Applications of CSS</h3></strong><br></div>
+            As mentioned before, CSS is one of the most widely used style language over the web. I'm going to list few of them here:
+            <ul>
+                <li><strong>CSS saves time</strong> - You can write CSS once and then reuse same sheet in multiple HTML pages. You can define a style for each HTML element and apply it to as many Web pages as you want.</li>
+                <li><strong>Pages load faster</strong> - If you are using CSS, you do not need to write HTML tag attributes every time. Just write one CSS rule of a tag and apply it to all the occurrences of that tag. So less code means faster download times.</li>
+                <li><strong>Easy maintenance</strong> - To make a global change, simply change the style, and all elements in all the web pages will be updated automatically.</li>
+                <li><strong>Superior styles to HTML</strong> - CSS has a much wider array of attributes than HTML, so you can give a far better look to your HTML page in comparison to HTML attributes.</li>
+                <li><strong>Multiple Device Compatibility</strong> - Style sheets allow content to be optimized for more than one type of device. By using the same HTML document, different versions of a website can be presented for handheld devices such as PDAs and cell phones or for printing.</li>
+                <li><strong>Global web standards</strong> - Now HTML attributes are being deprecated and it is being recommended to use CSS. So its a good idea to start using CSS in all the HTML pages to make them compatible to future browsers.</li>
+            </ul>
+            
+            <div style="text-align: center;"><br><strong><h3>Prerequisites</h3></strong><br></div>
+            You should be familiar with:<br>
+            <em>
+                <ul>
+                    <li>Basic word processing using any text editor.</li>
+                    <li>How to create directories and files.</li>
+                    <li>How to navigate through different directories.</li>
+                    <li>Internet browsing using popular browsers like Internet Explorer or Firefox.</li>
+                    <li>Developing simple Web Pages using HTML or XHTML.</li>
+                </ul>
+            </em>
             </p>
+            
             </div>
         </div>
         <hr>
